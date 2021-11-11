02498a6b291270c3386d7c73a522a445e3161b33
Josep Vidal <josepvidalvidal@gmail.com>
2021-11-11 21:06:08 +0100

    feat: app tests and snapshots

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
+import styles from "./index.module.css";
 
 const Home: NextPage = () => {
   return (
