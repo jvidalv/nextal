bea1650d6e4958fe15b099030f3d7367008ff1c9
Josep Vidal <josepvidalvidal@gmail.com>
2021-11-11 20:18:38 +0100

    fix(lint-staged): removed jest

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   "*.{ts,tsx,css}": ["prettier . --write"],
-  "*.{ts,tsx}": [
-    "eslint . --cache --fix --ext .tsx --ext .ts",
-    () => "yarn tsc",
-    () => "yarn jest",
-  ],
+  "*.{ts,tsx}": ["next lint . --cache --fix --ext .tsx --ext .ts", () => "tsc"],
 };
