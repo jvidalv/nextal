1d7e06830e046fc8830ae5fa8372a250958f0b91
Josep Vidal <josepvidalvidal@gmail.com>
2021-11-11 20:15:52 +0100

    fix(tailwind): files to parse for JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,6 @@ const forms = require("@tailwindcss/forms");
 
 module.exports = {
   mode: "jit",
-  purge: ["./*.html", "./src/**/*.{css}"],
+  purge: ['./pages/**/*.{css}', './components/**/*.{css}'],
   plugins: [forms],
 };
