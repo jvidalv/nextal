31fad35ac73d0fdb1aa30409867e7bac86667dba
Josep Vidal <josepvidalvidal@gmail.com>
2021-11-14 14:21:31 +0100

    fix: footer text color in dark mode

--- components/molecules/footer/footer.tsx
+++ components/molecules/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = (): JSX.Element => {
   return (
     <footer className={styles.footer}>
-      <a href="https://github.com/jvidalv">
+      <a className={styles.link} href="https://github.com/jvidalv">
         Josep Vidal @ {new Date().getFullYear()}
       </a>
     </footer>
