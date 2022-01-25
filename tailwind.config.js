8ef8167fafaf3b3bf3b07fe7abaece5144bb48bb
Josep Vidal <josepvidalvidal@gmail.com>
2022-01-25 09:02:32 +0100

    fix: updated purge property for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ const typography = require("@tailwindcss/typography");
 
 module.exports = {
   darkMode: "class",
-  purge: ["./src/**/*.css"],
+  contents: [
+    "./src/**/*.css"
+  ],
   plugins: [forms, typography],
 };
