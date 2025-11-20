d7b986bee54bcca0c44be3544bb1b92c7c568042
Josep Vidal <josepvidalvidal@gmail.com>
2025-11-20 12:48:59 +0100

    feat: upgrade Tailwind CSS to v4

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-const forms = require("@tailwindcss/forms");
-const typography = require("@tailwindcss/typography");
-
-module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.tsx"],
-  plugins: [forms, typography],
-};
